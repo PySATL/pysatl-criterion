c236c26f0119363e39ceabeee26289226224ef53
Кирилл Тахмазиди <tahmazidik@gmail.com>
2026-07-13 18:52:30 +0300

    test: extend Laplace goodness-of-fit tests

--- tests/distributions/test_laplace.py
+++ tests/distributions/test_laplace.py
@@ -10,6 +10,38 @@ from pysatl_criterion.statistics.goodness_of_fit.laplace import (
 
 
 _SAMPLE = [0.1, 0.2, 0.3]
+_LARGE_SAMPLE = [
+    -3.2,
+    -2.7,
+    -2.1,
+    -1.8,
+    -1.5,
+    -1.3,
+    -1.1,
+    -0.9,
+    -0.7,
+    -0.5,
+    -0.4,
+    -0.3,
+    -0.2,
+    -0.1,
+    0.0,
+    0.1,
+    0.2,
+    0.3,
+    0.4,
+    0.5,
+    0.7,
+    0.9,
+    1.1,
+    1.3,
+    1.5,
+    1.8,
+    2.1,
+    2.4,
+    2.8,
+    3.3,
+]
 _LOCATION = 0.0
 _SCALE = 1.0
 
@@ -57,6 +89,22 @@ def test_anderson_darling_laplace_statistic():
     result = statistic.execute_statistic(_SAMPLE)
 
     assert result == pytest.approx(1.0445557661164182)
+
+
+@pytest.mark.parametrize(
+    ("statistic_class", "expected_value"),
+    [
+        (KolmogorovSmirnovLaplaceGofStatistic, 0.10023112481762697),
+        (CramerVonMisesLaplaceGofStatistic, 0.06360683737580435),
+        (AndersonDarlingLaplaceGofStatistic, 0.4993990825244552),
+    ],
+)
+def test_laplace_statistics_on_large_sample(statistic_class, expected_value):
+    statistic = statistic_class(t=_LOCATION, s=_SCALE)
+
+    result = statistic.execute_statistic(_LARGE_SAMPLE)
+
+    assert result == pytest.approx(expected_value)
 
 
 @pytest.mark.parametrize(
