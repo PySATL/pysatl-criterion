cda0213c98493df77141887ff31a1d9f6970b78c
Mironov Alexey <32272908+alex98247@users.noreply.github.com>
2026-07-22 13:08:37 +0300

    Add get_available_criteria (#131)

--- src/pysatl_criterion/utils/statistic.py
+++ src/pysatl_criterion/utils/statistic.py
@@ -1,30 +1,54 @@
 import inspect
+from importlib import import_module
 from typing import Any
 
 from pysatl_criterion import DistributionType
 from pysatl_criterion.statistics import AbstractGoodnessOfFitStatistic
 
 
-def get_available_criteria(distribution: DistributionType) -> list[str]:
+def get_available_criteria(
+    distribution: DistributionType,
+) -> list[type[AbstractGoodnessOfFitStatistic]]:
+    """
+    Return a list of all non-abstract statistical criteria available for
+    the given distribution.
+
+    This function inspects all subclasses of AbstractGoodnessOfFitStatistic and filters out
+    abstract classes.
+
+    :param distribution: distribution descriptor to filter available statistical criteria.
+    :return: list of non-abstract subclasses of AbstractGoodnessOfFitStatistic for the
+    requested distribution.
+    """
+    __load_goodness_of_fit_statistics()
+
+    return [
+        cls
+        for cls in __get_all_subclasses(AbstractGoodnessOfFitStatistic)
+        if not inspect.isabstract(cls) and cls.distribution() == distribution
+    ]
+
+
+def get_available_criteria_codes(distribution: DistributionType) -> list[str]:
     """
     Return a list of short codes for all non-abstract statistical criteria available for
     the given distribution.
 
-    This function inspects all direct subclasses of the distribution's base_class and filters out
-    abstract classes.
-    For each concrete subclass, its short_code() method is invoked to obtain
-    a unique short identifier.
+    For each concrete criterion class, its short_code() method is invoked to obtain
+    a short identifier.
 
-    :param distribution: distribution descriptor whose base_class defines the root class for
-    available statistical criteria.
+    :param distribution: distribution descriptor to filter available statistical criteria.
     :return: list of short codes corresponding to all non-abstract subclasses of
-    distribution.base_class.
+    AbstractGoodnessOfFitStatistic for the requested distribution.
     """
-    return [
-        cls.short_code()
-        for cls in __get_all_subclasses(AbstractGoodnessOfFitStatistic)
-        if not inspect.isabstract(cls) and cls.distribution() == distribution
-    ]
+    return [criterion.short_code() for criterion in get_available_criteria(distribution)]
+
+
+def __load_goodness_of_fit_statistics() -> None:
+    """
+    Load goodness-of-fit statistic modules so subclasses are registered in Python runtime.
+    """
+    import_module("pysatl_criterion.statistics.goodness_of_fit")
 
 
 def __get_all_subclasses(cls: type[Any]) -> set[type[AbstractGoodnessOfFitStatistic]]:
