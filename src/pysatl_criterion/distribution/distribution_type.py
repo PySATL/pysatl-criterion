c8e225a83ac87a6b6e533ecb1dd0d9dbed0b4782
Aleksey <167619425+prcmium@users.noreply.github.com>
2026-07-26 16:28:43 +0300

    feat: add Pareto goodness-of-fit statistics  (#132)

--- src/pysatl_criterion/distribution/distribution_type.py
+++ src/pysatl_criterion/distribution/distribution_type.py
@@ -53,6 +53,9 @@ class DistributionType(Enum):
     SCALE_CON_NORMAL = "scale_con_normal"
     TRUNC_NORMAL = "trunc_normal"
     TUKEY = "tukey"
+    FISHER = "fisher"
+    RAYLEIGH = "rayleigh"
+    PARETO = "pareto"
 
     def __new__(cls, value: str):
         """
